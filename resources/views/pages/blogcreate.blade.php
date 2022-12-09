ac62f530ef48ab364e2179e8d04776194e275fa4
unknown <charmi.santoki@viitor.cloud>
2022-12-09 10:59:47 +0530

    change status field

--- resources/views/pages/blogcreate.blade.php
+++ resources/views/pages/blogcreate.blade.php
@@ -8,24 +8,24 @@
                 <div class="class-header">
                     <h3>Blog</h3>
                 </div>
-                    <div class="card-body">
-                        @if(Session()->has('success'))
-                        <div class="alert alert-success alert-dismissable fade show" role="alert">
-                            <strong>{{Session()->get('success')}}</strong>
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button> 
-                        </div>   
-                        @endif 
-                        @if(Session()->has('fail'))
-                        <div class="alert alert-danger alert-dismissable fade show" role="alert">
-                            <strong>{{Session()->get('fail')}}</strong>
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button> 
-                        </div>   
-                        @endif  
-                        <form method="POST" action="{{route('blogs.store')}}" name="blog">
+                <div class="card-body">
+                    @if(Session()->has('success'))
+                    <div class="alert alert-success alert-dismissable fade show" role="alert">
+                        <strong>{{Session()->get('success')}}</strong>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    @endif
+                    @if(Session()->has('fail'))
+                    <div class="alert alert-danger alert-dismissable fade show" role="alert">
+                        <strong>{{Session()->get('fail')}}</strong>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    @endif
+                    <form method="POST" action="{{route('blogs.store')}}" name="blog">
                         @csrf
                         <div class="form-group">
                             <label for="title">Title:</label>
@@ -40,7 +40,7 @@
                         </div>
                         @if($errors->has('description'))
                         <span class="text-danger">{{$errors->first('description')}}</span>
-                        @endif 
+                        @endif
                         <div class="form-group">
                             <label for="created_by">Created_by:</label>
                             <input type="text" class="form-control" id="created_by" name="created_by" placeholder="Enter Your created_by">
@@ -51,20 +51,20 @@
                         <label for="status">Status:</label>
 
                         <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="" id="status1">
-                            <label class="form-check-label" for="inactive">Active</label>
+                            <input type="checkbox" name="status" value="1">
+                            <label for="status1">Active</label>
                         </div>
-                         <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="" id="status2" checked>
-                            <label class="form-check-label" for="active">InActive</label>
+                        <div class="form-check">
+                            <input type="checkbox" name="status" value="0">
+                            <label for="status2"> InActive</label>
                         </div>
                         @if($errors->has('status'))
                         <span class="text-danger">{{$errors->first('status')}}</span>
                         @endif
-                        <button type="submit" id="user" class="btn btn-primary">Submit</button>               
-                    </div>      
-            </div>    
+                        <button type="submit" id="user" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
         </div>
-    </div>    
-</div>    
+    </div>
+</div>
 @endsection
